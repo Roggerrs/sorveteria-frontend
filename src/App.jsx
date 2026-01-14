39b6541faf77d2bedbed60e8d07109ccf47b4dd3
Roggerrs <rogger6412@gmail.com>
2026-01-14 16:13:25 -0300

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SelecionarAtendente from "./pages/SelecionarAtendente";
--- src/pages/CriarPedido.jsx
+++ src/pages/CriarPedido.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { listarTamanhos, listarSabores, criarPedido } from "../api/api";
