82563400453a8977f2d836e5d996b8d5a881f1b8
Roggerrs <rogger6412@gmail.com>
2026-03-01 11:47:58 -0300

    Correção selecionarAtendente

--- src/pages/SelecionarAtendente.jsx
+++ src/pages/SelecionarAtendente.jsx
@@ -21,7 +21,7 @@ export default function SelecionarAtendente() {
     }
     navigate(`/criar-pedido/${atendenteId}`);
   }
-
+/*
   return (
     <Box sx={{ maxWidth: 400, margin: "100px auto" }}>
       <Typography variant="h5" gutterBottom>
@@ -56,83 +56,9 @@ export default function SelecionarAtendente() {
       </Button>
     </Box>
   );
+  */
 }
-/*
- return (
-  <Box
-    sx={{
-      minHeight: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#242424",
-    }}
-  >
-    <Box
-      sx={{
-        width: "100%",
-        maxWidth: 400,
-        p: 4,
-        backgroundColor: "#2f2f2f",
-        borderRadius: 3,
-        boxShadow: "0 0 20px rgba(0,0,0,0.6)",
-      }}
-    >
-      <Typography
-        variant="h5"
-        gutterBottom
-        sx={{
-          textAlign: "center",
-          color: "#ffb347",
-          mb: 3,
-        }}
-      >
-        Selecione o atendente
-      </Typography>
 
-      <Select
-        fullWidth
-        value={atendenteId}
-        onChange={(e) => setAtendenteId(e.target.value)}
-        displayEmpty
-        sx={{
-          mb: 3,
-          backgroundColor: "#242424",
-          color: "#fff",
-          borderRadius: 2,
-        }}
-      >
-        <MenuItem value="">
-          <em>Selecione...</em>
-        </MenuItem>
-
-        {atendentes.map((a) => (
-          <MenuItem key={a.id} value={a.id}>
-            {a.nome}
-          </MenuItem>
-        ))}
-      </Select>
-
-      <Button
-        fullWidth
-        variant="contained"
-        sx={{
-          py: 1.5,
-          fontWeight: "bold",
-          fontSize: "16px",
-          backgroundColor: "#ff9800",
-          "&:hover": {
-            backgroundColor: "#ffb347",
-          },
-        }}
-        onClick={continuar}
-      >
-        CONTINUAR
-      </Button>
-    </Box>
-  </Box>
-);  
-*/
 return (
   <Box
     sx={{
